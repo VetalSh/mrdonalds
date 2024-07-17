cb77366f43c00db547da472e6cb16cc8a75f6aae
vitaliiSh <vitalii.shkar@gmail.com>
2024-07-17 15:45:47 +0300

    Menu added

---
+++ src/Components/Banner.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+import bannerImg from "../image/banner.png";
+
+export const Banner = styled.div`
+    background-image: url(${bannerImg});
+    background-position: center;
+    background-size: cover;
+    width: 100%;
+    height: 200px;
+`;
---
+++ src/Components/GlobalStyle.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+  }
+
+  *, *::before,
+  *::after {
+    box-sizing: inherit;
+  }
+
+  body {
+    margin:0;
+    background-color: #f0f0f0;
+    font-family: "Roboto", sans-serif;
+    font-weight: 400;
+    font-style: normal;
+    font-size: 20px;
+    color: black;
+  }
+
+  img {
+    max-width: 100%;
+    height: auto;
+  }
+
+  button {
+    display: flex;
+    padding: 8px 16px;
+    align-items: center;
+    gap: 4px;
+    outline: none;
+    cursor: pointer;
+  }
+
+  input, button {
+    font-family: inherit;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
+
+  h1, h2, h3 {
+    padding: 0;
+    margin: 0;
+    font-family: "Pacifico";
+  }
+
+  p {
+    padding: 0;
+    margin: 0;
+  }
+`;

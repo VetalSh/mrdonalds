bb3d3331e985851d7140bf75e41f74f09b452c5c
vitaliiSh <vitalii.shkar@gmail.com>
2024-07-19 16:12:39 +0300

    Modal added

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -14,16 +14,18 @@ const SectionMenu = styled.section`
 `;
 
 
-export const Menu = () => (
+export const Menu = ({ setOpenItem }) => (
     <MenuStyled>
         <Banner />
         <SectionMenu>
             <h2>Burgers</h2>
-            <ListItem itemList={dbMenu.burger} />
+            <ListItem itemList={dbMenu.burger}
+                setOpenItem={setOpenItem} />
         </SectionMenu>
         <SectionMenu>
             <h2>Snacks / Drinks</h2>
-            <ListItem itemList={dbMenu.other} />
+            <ListItem itemList={dbMenu.other}
+                setOpenItem={setOpenItem} />
         </SectionMenu>
     </MenuStyled>
 );
